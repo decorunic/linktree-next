779e03ff59c046f20aa62c8a510fe34196dabdb4
webdecorunic <webdecorunic@gmail.com>
2022-09-23 11:21:34 +0700

    setup tailwind and base color

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        dark: 'rgb(var(--color-dark) / <alpha-value>)',
+        light: 'rgb(var(--color-light) / <alpha-value>)',
+      },
+    },
+  },
+  plugins: [],
+};
