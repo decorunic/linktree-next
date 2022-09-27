645d3ca6a426920fd37505e77b87d5a599d8b56a
webdecorunic <webdecorunic@gmail.com>
2022-09-27 15:55:23 +0700

    feat: install mysql, knex, and setup datatabase

---
+++ migrations/20220927084628_create_links_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('links' , function(table) {
+    table.increments('id').primary();
+    table.string('name', 255).notNullable();
+    table.string('url', 255).notNullable();
+    table.string('type', 255).notNullable();
+    table.string('icon', 255).notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('links');
+};
