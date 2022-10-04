e8e56b185946a17f725fd87becc10baf33a3a10a
webdecorunic <webdecorunic@gmail.com>
2022-10-04 14:01:31 +0700

    feat: making JWT token header authorization and verification JWT token

--- pages/api/links/index.js
+++ pages/api/links/index.js
@@ -1,13 +1,33 @@
 import db from '../../../libs/db';
+import jwt from 'jsonwebtoken';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') return res.status(405).end();
 
-  const links = await db('links').select('*');
+  const { authorization } = req.headers;
+
+  if (!authorization) return res.status(401).end();
   
-  res.status(200);
-  res.json({
-    message: 'Success get data',
-    data: links
-  });
+  const authSplit = authorization.split(' ');
+  console.log(authSplit);
+  const [authType, token] = [
+    authSplit[0],
+    authSplit[1]
+  ];
+
+  if (authType !== 'Bearer') return res.status(401).end();
+  
+  try {
+    const verifyToken = jwt.verify(token, 'secret');
+
+    const links = await db('links').select('*');
+    
+    res.status(200);
+    res.json({
+      message: 'Success get data',
+      data: links
+    });
+  } catch (error) {
+    res.status(401).end();
+  }
 }
