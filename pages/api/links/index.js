16773025dc4209200cdddb2ad2e2a8246348bb2f
webdecorunic <webdecorunic@gmail.com>
2022-10-04 14:22:39 +0700

    refactor: change authorization to promise

--- pages/api/links/index.js
+++ pages/api/links/index.js
@@ -1,33 +1,18 @@
 import db from '../../../libs/db';
-import jwt from 'jsonwebtoken';
+import authorization from '../../../middlewares/authorization';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') return res.status(405).end();
-
-  const { authorization } = req.headers;
-
-  if (!authorization) return res.status(401).end();
   
-  const authSplit = authorization.split(' ');
-  console.log(authSplit);
-  const [authType, token] = [
-    authSplit[0],
-    authSplit[1]
-  ];
+  const auth = await authorization(req, res);
 
-  if (authType !== 'Bearer') return res.status(401).end();
+  console.log(auth);
+
+  const links = await db('links').select('*');
   
-  try {
-    const verifyToken = jwt.verify(token, 'secret');
-
-    const links = await db('links').select('*');
-    
-    res.status(200);
-    res.json({
-      message: 'Success get data',
-      data: links
-    });
-  } catch (error) {
-    res.status(401).end();
-  }
+  res.status(200);
+  res.json({
+    message: 'Success get data',
+    data: links
+  });
 }
