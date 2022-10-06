f4eb3ede54089be4f4e4fe513d330bd758eea872
virgiawankusuma <virgiawan150@gmail.com>
2022-10-06 13:40:13 +0700

    init

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -29,7 +29,7 @@ export default function Admin(props) {
       <div className="container justify-center items-center my-10">
         <div className="flex flex-col items-center justify-center w-full h-full">
           <h1 className="text-4xl font-bold">Admin</h1>
-          <p className="text-lg">Welcome to admin page</p>
+          <p className="text-lg">Welcome to admin page!</p>
         </div>
 
         {/* add new link button*/}
