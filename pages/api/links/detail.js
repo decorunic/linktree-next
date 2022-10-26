d206734313424f3b6af47effc4a8463125b3da76
virgiawankusuma <virgiawan150@gmail.com>
2022-10-26 10:57:47 +0700

    fix detail link api

--- pages/api/links/detail.js
+++ pages/api/links/detail.js
@@ -1,18 +1,18 @@
 import db from '../../../libs/db';
-import { authorizationPage } from '../../../middlewares/authorizationPage';
+import authorization from '../../../middlewares/authorization';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') return res.status(405).end();
   
-  const auth = await authorizationPage(req, res);
-  
   const { id } = req.query;
+  
+  const auth = await authorization(req, res);
 
   const link = await db('links')
                       .where({ id })
                       .first();
   
-if(!post) return res.status(404).end();
+  if(!link) return res.status(404).end();
 
   res.status(200);
   res.json({
