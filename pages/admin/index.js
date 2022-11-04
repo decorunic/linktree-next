f5bc7d4ecdbdc93162ebbff29b6e2dccd68e2593
virgiawankusuma <virgiawan150@gmail.com>
2022-11-04 14:45:56 +0700

    change admin layout

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -55,10 +55,17 @@ export default function Admin(props) {
     }
   }
 
-  function editHandler(id, e) {
+  const editHandler = (id, e) => {
     e.preventDefault();
 
     Router.push('/admin/edit?id=' + id);
+  }
+
+  const copyToClipboard = (url, e) => {
+    e.preventDefault();
+    navigator.clipboard.writeText(url);
+
+    alert('URL Copied to clipboard!');
   }
 
   return (
@@ -87,94 +94,108 @@ export default function Admin(props) {
             socialLinks.map((item, index) => (
               <div 
                 key={index}
-                className="w-1/2 md:w-[calc((100%/5)-2.5rem)] lg:w-[calc((100%/4)-3.5rem)] text-lg px-5 py-3 border border-primary/40 rounded-full bg-white shadow-md shadow-dark/10 md:text-xl md:p-5 transition-all duration-200 ease-in-out"
+                className="flex items-center justify-between gap-2 w-full sm:w-[calc(50%-8px)] md:w-[calc(100%/3-12px)] lg:w-[calc(100%/4-12px)] text-lg px-5 py-3 border border-primary/40 rounded-full bg-white shadow-md shadow-dark/10 md:text-xl md:p-5 transition-all duration-200 ease-in-out"
               >
-                <div className="flex items-center justify-between gap-2">
+                <a 
+                  href={item.url}
+                  className="cursor-pointer hover:text-secondary active:text-secondary/60"
+                  onClick={copyToClipboard.bind(this, item.url)}
+                >
                   <DynamicFaIcon
-                    icon={item.icon}
+                    name={item.icon}
                     className="text-2xl md:text-3xl"
                   />
-                  <div>
-                    <h3 className="font-bold text-lg">{item.name}</h3>
-                  </div>
-                  <div className="flex gap-1">
-                    <button
-                      onClick={editHandler.bind(this, item.id)}
-                      className="bg-primary/30 text-sm p-2 rounded-full transition-all duration-100 hover:bg-primary/50"
-                    >
-                      <DynamicFaIcon name="FaPen" />
-                    </button>
-                    <button
-                      onClick={deleteHandler.bind(this, item.id)}
-                      className="bg-red-100 text-sm p-2 rounded-full transition-all duration-100 hover:bg-red-200"
-                    >
-                      <DynamicFaIcon name="FaTrash" />
-                    </button>
-                  </div>
+                </a>
+                <h3 className="font-bold text-lg md:text-base">{item.name}</h3>
+                <div className="flex gap-1">
+                  <button
+                    onClick={editHandler.bind(this, item.id)}
+                    className="bg-primary/30 text-xs md:text-sm p-2 rounded-full transition-all duration-100 hover:bg-primary/50"
+                  >
+                    <DynamicFaIcon name="FaPen" />
+                  </button>
+                  <button
+                    onClick={deleteHandler.bind(this, item.id)}
+                    className="bg-red-100 text-xs md:text-sm p-2 rounded-full transition-all duration-100 hover:bg-red-200"
+                  >
+                    <DynamicFaIcon name="FaTrash" />
+                  </button>
                 </div>
               </div>
             ))
           }
         </div>
         <h2 className="text-center text-xl font-bold mt-12 mb-5">General</h2>
-        <div className="flex flex-wrap justify-center gap-5 md:gap-y-5 md:gap-x-10 lg:gap-x-14">
+        <div className="flex flex-wrap justify-center gap-5 md:gap-y-5 md:gap-x-5 lg:gap-x-8">
           {
             generalLinks.map((item, index) => (
               <div 
                 key={index}
-                className="w-full md:w-[calc(50%-2.5rem)] lg:w-[calc(50%-3.5rem)] text-lg px-5 py-3 border border-primary/40 rounded-sm bg-white shadow-md shadow-dark/10 md:text-xl md:p-5 transition-all duration-200 ease-in-out"
+                className="flex items-center justify-between w-full md:w-[calc(50%-1.25rem)] lg:w-[calc(50%-2rem)] text-lg px-5 py-3 border border-primary/40 rounded-sm bg-white shadow-md shadow-dark/10 md:text-xl md:p-5 transition-all duration-200 ease-in-out"
               >
-                <div className="flex items-center">
-                  <div>
-                    <h3 className="font-bold text-lg">{item.name}</h3>
-                    <p className="text-sm">{item.url}</p>
-                  </div>
-                  <div className="flex items-end justify-center ml-auto gap-2">
-                    <button
-                      onClick={editHandler.bind(this, item.id)}
-                      className="bg-primary/30 text-sm p-2 rounded-full transition-all duration-100 hover:bg-primary/50"
+                <div>
+                  <h3 className="font-bold text-lg">{item.name}</h3>
+                  <div className="overflow-hidden overflow-ellipsis whitespace-nowrap w-48 md:w-56 lg:w-80">
+                    <a 
+                      href={item.url}
+                      className="text-sm cursor-pointer hover:text-secondary active:text-secondary/60"
+                      onClick={copyToClipboard.bind(this, item.url)}
                     >
-                      <DynamicFaIcon name="FaPen" />
-                    </button>
-                    <button
-                      onClick={deleteHandler.bind(this, item.id)}
-                      className="bg-red-100 text-sm p-2 rounded-full transition-all duration-100 hover:bg-red-200"
-                    >
-                      <DynamicFaIcon name="FaTrash" />
-                    </button>
+                      {item.url}
+                    </a>
                   </div>
+                </div>
+                <div className="flex items-center gap-2">
+                  <button
+                    onClick={editHandler.bind(this, item.id)}
+                    className="bg-primary/30 text-xs md:text-sm p-2 rounded-full transition-all duration-100 hover:bg-primary/50"
+                  >
+                    <DynamicFaIcon name="FaPen" />
+                  </button>
+                  <button
+                    onClick={deleteHandler.bind(this, item.id)}
+                    className="bg-red-100 text-xs md:text-sm p-2 rounded-full transition-all duration-100 hover:bg-red-200"
+                  >
+                    <DynamicFaIcon name="FaTrash" />
+                  </button>
                 </div>
               </div>
             ))
           }
         </div>
         <h2 className="text-center text-xl font-bold mt-12 mb-5">Marketplace</h2>
-        <div className="flex flex-wrap justify-center gap-5 md:gap-y-5 md:gap-x-10 lg:gap-x-14">
+        <div className="flex flex-wrap justify-center gap-5 md:gap-y-5 md:gap-x-5 lg:gap-x-8">
           {
             marketplaceLinks.map((item, index) => (
               <div 
                 key={index}
-                className="w-full md:w-[calc(50%-2.5rem)] lg:w-[calc(50%-3.5rem)] text-lg px-5 py-3 border border-primary/40 rounded-sm bg-white shadow-md shadow-dark/10 md:text-xl md:p-5 transition-all duration-200 ease-in-out"
+                className="flex items-center justify-between w-full md:w-[calc(50%-1.25rem)] lg:w-[calc(50%-2rem)] text-lg px-5 py-3 border border-primary/40 rounded-sm bg-white shadow-md shadow-dark/10 md:text-xl md:p-5 transition-all duration-200 ease-in-out"
               >
-                <div className="flex items-center">
-                  <div>
-                    <h3 className="font-bold text-lg">{item.name}</h3>
-                    <p className="text-sm">{item.url}</p>
-                  </div>
-                  <div className="flex items-end justify-center ml-auto gap-2">
-                    <button
-                      onClick={editHandler.bind(this, item.id)}
-                      className="bg-primary/30 text-sm p-2 rounded-full transition-all duration-100 hover:bg-primary/50"
+                <div>
+                  <h3 className="font-bold text-lg">{item.name}</h3>
+                  <div className="overflow-hidden overflow-ellipsis whitespace-nowrap w-48 md:w-56 lg:w-80">
+                    <a 
+                      href={item.url}
+                      className="text-sm cursor-pointer hover:text-secondary active:text-secondary/60"
+                      onClick={copyToClipboard.bind(this, item.url)}
                     >
-                      <DynamicFaIcon name="FaPen" />
-                    </button>
-                    <button
-                      onClick={deleteHandler.bind(this, item.id)}
-                      className="bg-red-100 text-sm p-2 rounded-full transition-all duration-100 hover:bg-red-200"
-                    >
-                      <DynamicFaIcon name="FaTrash" />
-                    </button>
+                      {item.url}
+                    </a>
                   </div>
+                </div>
+                <div className="flex items-center gap-2">
+                  <button
+                    onClick={editHandler.bind(this, item.id)}
+                    className="bg-primary/30 text-xs md:text-sm p-2 rounded-full transition-all duration-100 hover:bg-primary/50"
+                  >
+                    <DynamicFaIcon name="FaPen" />
+                  </button>
+                  <button
+                    onClick={deleteHandler.bind(this, item.id)}
+                    className="bg-red-100 text-xs md:text-sm p-2 rounded-full transition-all duration-100 hover:bg-red-200"
+                  >
+                    <DynamicFaIcon name="FaTrash" />
+                  </button>
                 </div>
               </div>
             ))
